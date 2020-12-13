ec2799c967e49fb70b1c36060fb847a55f6c8523
Gregorio <gregorio.piriz@hotmail.com>
2020-12-12 21:28:57 -0300

    sadasssss

--- Obligatorio/src/index.java
+++ Obligatorio/src/index.java
@@ -1,13 +1,13 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Stack;
 
 public class index {
 
 	public static void main(String[] args) {
 
-
-
-		ArbolB nuevo = new ArbolB();
+		ArbolB arbol = new ArbolB();
 		NodoB nuevonodo = new NodoB(0, "G");// G
 		NodoB nodoM = new NodoB(1, "M");// M
 		NodoB nodoE = new NodoB(2, "E");// E
@@ -34,17 +34,48 @@ public class index {
 		NodoB nodoH = new NodoB(11, "H");// H
 		nodoJ.setNodoIzq(nodoH);
 
-		nuevo.setRaiz(nuevonodo);
+		arbol.setRaiz(nuevonodo);
 
-		imprimirPostorden(nuevo.getRaiz());
-		System.out.println("SEPARANDO ESTA MIERDA");
+		//Esto es para el ej 3 para ver si estan los caminos dentro del arbol
+		//listas que se cargan mediante funcion recursiva segun el orden que queremos obtener del arbol
+		ArrayList<String> preorden = new ArrayList<String>();
+		ArrayList<String> inorden = new ArrayList<String>();
+		ArrayList<String> postorden = new ArrayList<String>();
+
+		System.out.println("Imprimir Preorden");
+		imprimirPreorden(arbol.getRaiz(), preorden);
+		System.out.println("Imprimir Array Preorden");
+		for (int i = 0; i < preorden.size(); i++) {
+			System.out.println(preorden.get(i));
+		}
+
+		System.out.println("Imprimir Inorden");
+		imprimirInorden(arbol.getRaiz(), inorden);
+		System.out.println("Imprimir Array Inorden");
+		for (int i = 0; i < inorden.size(); i++) {
+			System.out.println(inorden.get(i));
+		}
+
+		System.out.println("Imprimir Postorden");
+		imprimirPostorden(arbol.getRaiz(), postorden);
+		System.out.println("Imprimir Array Postorden");
+		for (int i = 0; i <postorden.size(); i++) {
+			System.out.println(postorden.get(i));
+		}
+
+		/*
+		System.out.println("1-B Listar en Preorden");
+		imprimirItPostorden(arbol);
+		System.out.println("1-B Listar en Inorden");
+		imprimirItInorden(arbol);
+		System.out.println("SEPARANDO");
 
 		char preorden[] = {'G','E','A','I','B','M','C','L','D','F','K','J','H'};
 		char inorden[] = {'I','A','B','E','G','L','D','C','F','M','K','H','J'};
-        int len = inorden.length; 
-        ImprimirPostorden tree = new ImprimirPostorden(); 
-		tree.printPost(inorden, preorden, 0, len - 1); 
-		
+        int len = inorden.length;
+        ImprimirPostorden tree = new ImprimirPostorden();
+		tree.printPost(inorden, preorden, 0, len - 1);
+		*/
 		// listarItPostorden(nuevo.getRaiz());
 
 		// imprimirPosorden(nuevo);
@@ -81,153 +112,108 @@ public class index {
 
 	}
 
-	/*
-	 * public static void listarItPreorden(ArbolB arbol) { Stack<NodoB> pila = new
-	 * Stack<NodoB>();
-	 * 
-	 * NodoB aux = new NodoB(); aux=arbol.getRaiz();
-	 * System.out.println(aux.getNombre()); while(!pila.isEmpty() || aux!=null)
-	 * //Mientras que la pila no sea vacia o el auxiliar no sea nulo { if(aux==null)
-	 * { System.out.println(pila.lastElement().getNombre()); aux=pila.lastElement();
-	 * pila.pop(); aux=aux.getNodoDer(); } else { pila.push(aux);
-	 * aux=aux.getNodoIzq(); } } }
-	 */
-	public static char[] addElement(char[] a, char e) {
+	/*public static char[] addElement(char[] a, char e) {
 		a = Arrays.copyOf(a, a.length + 1);
 		a[a.length - 1] = e;
 		return a;
-	}
-
-	public static void listarItPostorden(NodoB root) {
-		if (root == null) {
-			return;
-		}
-		Stack<NodoB> stack = new Stack<NodoB>();
-		stack.push(root);
-		while (!stack.isEmpty()) {
-			NodoB next = stack.peek();
-			if (next.getNodoDer() == root || next.getNodoIzq() == root
-					|| (next.getNodoIzq() == null && next.getNodoDer() == null)) {
-				stack.pop();
-				System.out.println(next.getNombre());
-				root = next;
-			} else {
-				if (next.getNodoDer() != null) {
-					stack.push(next.getNodoDer());
-				}
-				if (next.getNodoIzq() != null) {
-					stack.push(next.getNodoIzq());
-				}
-			}
-		}
-	}
-
-	public static void listarItInorden(NodoB root) {
-		if (root == null) {
-			return;
-		}
-		Stack<NodoB> stack = new Stack<NodoB>();
-		stack.push(root);
-		while (!stack.isEmpty()) {
-			NodoB next = stack.peek();
-			if (next.getNodoDer() == root || next.getNodoIzq() == root
-					|| (next.getNodoIzq() == null && next.getNodoDer() == null)) {
-				stack.pop();
-				System.out.println(next.getNombre());
-				root = next;
-			} else {
-				if (next.getNodoDer() != null) {
-					stack.push(next.getNodoDer());
-				}
-				if (next.getNodoIzq() != null) {
-					stack.push(next.getNodoIzq());
-				}
-			}
-		}
-	}
-
-	public static void imprimirPreorden(NodoB reco) {
-		if (reco != null) {
-			System.out.println(reco.getNombre() + " ");
-			imprimirPreorden(reco.getNodoIzq());
-			imprimirPreorden(reco.getNodoDer());
-		}
-	}
-
-	public static void imprimirPostorden(NodoB reco) {
-		if (reco != null) {
-			imprimirPostorden(reco.getNodoIzq());
-			imprimirPostorden(reco.getNodoDer());
-			System.out.println(reco.getNombre() + " ");
-		}
-	}
-
-	public static void imprimirInorden(NodoB auxRaiz) {
-		if (auxRaiz != null) {
-			imprimirInorden(auxRaiz.getNodoIzq());
-			System.out.println(auxRaiz.getNombre() + " ");
-			imprimirInorden(auxRaiz.getNodoDer());
-		}
-	}
-	/*
-	static void printPostOrder(int in1[], int pre[], int n) {
-		// The first element in pre[] is
-		// always root, search it in in[]
-		// to find left and right subtrees
-		int root = search(in1, pre[0], n);
-
-		// If left subtree is not empty,
-		// print left subtree
-		if (root != 0) {
-			printPostOrder(in1, Arrays.copyOfRange(pre, 1, n), root);
-		}
-
-		// If right subtree is not empty,
-		// print right subtree
-		if (root != n - 1) {
-			printPostOrder(Arrays.copyOfRange(in1, root + 1, n), Arrays.copyOfRange(pre, 1 + root, n), n - root - 1);
-		}
-
-		// Print root
-		System.out.print(pre[0] + " ");
-	}
-
-	// A utility function to search x in arr[] of size n
-	static int search(int arr[], int x, int n) {
-		for (int i = 0; i < n; i++)
-			if (arr[i] == x)
-				return i;
-		return -1;
 	}*/
 
-	/*
-	 * public static char[] encontrarArbolDer(char raiz, char[] inorden) { char
-	 * arbolDer[] = {}; boolean pasoRaiz = false; for (char i = 0; i <
-	 * inorden.length; i++) { if(pasoRaiz){ arbolDer = addElement(arbolDer, i); }
-	 * if(pasoRaiz == false){ if(raiz == i){ pasoRaiz = true; } } } return arbolDer;
-	 * } public static char[] encontrarArbolIzq(char raiz, char[] inorden) { char
-	 * arbolIzq[] = {}; boolean pasoRaiz = false; for (char i = 0; i <
-	 * inorden.length; i++) { if(raiz == i){ pasoRaiz = true; } if(pasoRaiz ==
-	 * false){ arbolIzq = addElement(arbolIzq, i); } } return arbolIzq; } public
-	 * static char[] encontrarOrdenArbolDer(char raiz, char[] preorden) { char
-	 * arbolDer[] = {}; boolean pasoRaiz = false; for (char i = 1; i <
-	 * preorden.length; i++) { if(pasoRaiz){ arbolDer = addElement(arbolDer, i); }
-	 * if(pasoRaiz == false){ if(raiz == i){ pasoRaiz = true; } } } return arbolDer;
-	 * } public static char[] encontrarOrdenArbolIzq(char raiz, char[] preorden) {
-	 * char arbolIzq[] = {}; boolean pasoRaiz = false; for (char i = 1; i <
-	 * preorden.length; i++) { if(raiz == i){ pasoRaiz = true; } if(pasoRaiz ==
-	 * false){ arbolIzq = addElement(arbolIzq, i); } } return arbolIzq; }/*
-	 * 
-	 * 
-	 * /*public static ArbolB crearArbol(char[] preorden, char[] inorden ) { ArbolB
-	 * arbol = new ArbolB(); arbol.agregarNodo(0, preorden[0]);
-	 * 
-	 * //char subDerecho[] = encontrarArbolDer(preorden[0], inorden);
-	 * 
-	 * 
-	 * 
-	 * return null; }
-	 */
+	/*private static char[] addElement(char[] array, char push) {
+		char[] longer = new char[array.length + 1];
+		for (int i = 0; i < array.length; i++)
+			longer[i] = array[i];
+		longer[array.length] = push;
+		return longer;
+	}*/
+
+	public static void imprimirArray(char[] a) {
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(i);
+		}
+	}
+
+	//1B POSTORDEN TERMINADA
+	public static void imprimirItPostorden(ArbolB arbol) {
+		NodoB raiz = arbol.getRaiz();
+		if (raiz == null) {
+			return;
+		}
+		Stack<NodoB> stack = new Stack<NodoB>();
+		stack.push(raiz);
+		while (!stack.isEmpty()) {
+			NodoB siguiente = stack.peek();
+			if (siguiente.getNodoDer() == raiz || siguiente.getNodoIzq() == raiz
+					|| (siguiente.getNodoIzq() == null && siguiente.getNodoDer() == null)) {
+				stack.pop();
+				System.out.println(siguiente.getNombre());
+				raiz = siguiente;
+			} else {
+				if (siguiente.getNodoDer() != null) {
+					stack.push(siguiente.getNodoDer());
+				}
+				if (siguiente.getNodoIzq() != null) {
+					stack.push(siguiente.getNodoIzq());
+				}
+			}
+		}
+	}
+
+	//1B INORDEN TERMINADA
+	public static void imprimirItInorden(ArbolB arbol)
+    {
+        Stack<NodoB> pila = new Stack<NodoB>(); 
+
+        NodoB aux = new NodoB();
+        aux=arbol.getRaiz();
+        while(!pila.isEmpty() || aux!=null)
+        {
+            if(aux==null)
+            {
+                System.out.println(pila.lastElement().getNombre());
+                aux=pila.lastElement();
+                pila.pop();
+                aux=aux.getNodoDer();
+            }
+            else
+            {
+                pila.push(aux);
+                aux=aux.getNodoIzq();
+            }
+        }
+	}
+	
+	public static void imprimirPreorden(NodoB auxRaiz, ArrayList<String> lista) {
+		if (auxRaiz != null) {
+			char c=auxRaiz.getNombre();
+			String s=Character.toString(c);
+			lista.add(s);
+			System.out.println(auxRaiz.getNombre() + " ");
+			imprimirPreorden(auxRaiz.getNodoIzq(), lista);
+			imprimirPreorden(auxRaiz.getNodoDer(), lista);
+		}
+	}
+
+	public static void imprimirPostorden(NodoB auxRaiz, ArrayList<String> lista) {
+		if (auxRaiz != null) {
+			imprimirPostorden(auxRaiz.getNodoIzq(), lista);
+			imprimirPostorden(auxRaiz.getNodoDer(), lista);
+			char c=auxRaiz.getNombre();
+			String s=Character.toString(c);
+			lista.add(s);
+			System.out.println(auxRaiz.getNombre() + " ");
+		}
+	}
+
+	public static void imprimirInorden(NodoB auxRaiz, ArrayList<String> lista) {
+		if (auxRaiz != null) {
+			imprimirInorden(auxRaiz.getNodoIzq(), lista);
+			char c=auxRaiz.getNombre();
+			String s=Character.toString(c);
+			lista.add(s);
+			System.out.println(auxRaiz.getNombre() + " ");
+			imprimirInorden(auxRaiz.getNodoDer(), lista);
+		}
+	}
 
 	public static ArbolB insertar(ArbolB arbol, int x) {
 		// Si el arbol es vac�o inserto el valor como ra�z
